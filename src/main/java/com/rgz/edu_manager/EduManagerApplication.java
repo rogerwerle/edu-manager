98cd7e2fec2874a4a5b863a4421062bfc3bde409
rogerwerle <mwerleroger@gmail.com>
2025-06-11 20:30:50 -0300

    first commit

---
+++ src/main/java/com/rgz/edu_manager/EduManagerApplication.java
@@ -0,0 +1,13 @@
+package com.rgz.edu_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduManagerApplication.class, args);
+	}
+
+}
